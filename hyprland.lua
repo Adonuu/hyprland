0799f131857d7db64c2622d3e5ca185d1d22883f
Adam Young <adamyoung1414@gmail.com>
2026-07-22 21:08:37 -0500

    add breeze cursor config

--- hyprland.lua
+++ hyprland.lua
@@ -60,7 +60,9 @@ hl.on("hyprland.start", function ()
 
 -- See https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
 
+hl.env("XCURSOR_THEME", "breeze_cursors")
 hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_THEME", "breeze_cursors")
 hl.env("HYPRCURSOR_SIZE", "24")
 
 
