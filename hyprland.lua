c1988379dccfcf7c925d3cb5cda098454db37689
Adam Young <adamyoung1414@gmail.com>
2026-07-26 10:43:50 -0500

    update border rounding

--- hyprland.lua
+++ hyprland.lua
@@ -112,7 +112,7 @@ hl.config({
     },
 
     decoration = {
-        rounding       = 10,
+        rounding       = 5,
         rounding_power = 2,
 
         -- Change transparency of focused and unfocused windows
